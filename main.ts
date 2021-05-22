61ce96992ef50f8411b72788e32f0a9ab8688d72
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-05-22 10:14:20 +0200

    init

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+let factor1 = 0
+let factor2 = 0
+basic.forever(function () {
+    factor1 = randint(2, 9)
+    factor2 = randint(2, 9)
+    basic.showNumber(factor1)
+    basic.showString("x")
+    basic.showNumber(factor2)
+    basic.pause(2000)
+    basic.showString("=")
+    basic.showNumber(factor1 * factor2)
+    basic.pause(1000)
+})
